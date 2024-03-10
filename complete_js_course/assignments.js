c031af2778ef422655c5e0a13c9dccfce2c67a2c
Arion <108494937+ArionGresser@users.noreply.github.com>
2024-03-10 23:59:35 +0000

    adding the new course and separating in folders

---
+++ complete_js_course/assignments.js
@@ -0,0 +1,28 @@
+// 1. Values and Variables
+
+// 1.1 Declare variables called country, continent and population and assign their values according to your own country (population in millions).
+
+// 1.2 Log their values to the console.
+
+let country = "Croatia";
+let continent = "Europe";
+let population = "3.9";
+let isIsland = false;
+let language;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+console.log(isIsland);
+console.log(language);
+
+// 2. Data Types
+
+// 2.1 Declare a variable called isIsland and set its value according to your country. The variable should hold a Boolean value. Also declare a variable language, but don't assign it any value yet.
+
+// 2.2 Log the types of isIsland, population, country and language to the console.
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
---
+++ complete_js_course/script.js
@@ -0,0 +1,20 @@
+// let js = "amazing";
+// console.log(40 + 8 + 23 - 10);
+
+// console.log("Arion");
+// console.log("Bruna");
+
+// let firstName = "Arion";
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+
+// console.log(typeof true);
+// console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof "Arion");
+
+
