47dfd162b4da90b9fbd61ba6b27d7ab0e5002690
Arion <108494937+ArionGresser@users.noreply.github.com>
2024-12-30 17:19:59 +0000

    Atualizando para dar continuidade

--- complete_js_course/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete_js_course/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,9 +1,43 @@
-    let js = 'amazing';
-    console.log(40 + 8 + 23 - 10);
+    // let js = 'amazing';
+    // console.log(40 + 8 + 23 - 10);
 
-    console.log('Jonas');
-    console.log(23);
+    // console.log('Jonas');
+    // console.log(23);
 
-    let firstName = "Jonas";
-    console.log(firstName);
+    // let firstName = "Jonas";
+    // console.log(firstName);
+
+    // let javascriptIsFun = true;
+    // console.log(javascriptIsFun)
     
+    // console.log(typeof javascriptIsFun)
+    
+    // Assignments 
+
+    //1. Variables and Values
+    //Declare variables called country, continent and population and assign their values according to your own country (population in millions). Log their values to the console.
+
+    // let country = "Brasil";
+    // let continent = "America";
+    // let population = 250000000;
+    // console.log(country);
+    // console.log(continent);
+    // console.log(population);
+
+
+
+    //2. Data Types
+   //Declare a variable called isIsland and set its value according to your country. The variable should hold a Boolean value. Also declare a variable language, but don't assign it any value yet. 
+   // Log the types of isIsland, population, country and language to the console.
+
+//    let isIsland = false;
+//    let language
+
+//    console.log(typeof isIsland)
+//    console.log(typeof population)
+//    console.log(typeof country)
+//    console.log(typeof language)
+
+// const 
+
+   
