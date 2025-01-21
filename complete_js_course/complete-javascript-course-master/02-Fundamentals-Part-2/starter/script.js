ff75e5bbffe12e0da3e5287cdb6fd518589dcfa2
Arion <108494937+ArionGresser@users.noreply.github.com>
2025-01-21 16:07:48 +0000

    att course

--- complete_js_course/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete_js_course/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -76,3 +76,48 @@
 // console.log(yearsUntilRetirement(2002, 'Arion'));
 // console.log(yearsUntilRetirement(1960, 'Helion'));
 
+// const friends = ['Halison', 'Eduardo', 'Luigi'];
+// console.log(friends)
+
+// // const years = new Array(1994, 2001, 2002, 2005);
+
+// console.log(friends[0]);
+// console.log(friends[2])
+
+// console.log(friends.length);
+// console.log(friends[friends.length-1]);
+
+// friends[2] = 'Helion';
+// console.log(friends);
+
+// const arion = ['Arion', 'Gresser', 2037 - 2002, 'HelpDesk', friends];
+
+// console.log(arion);
+
+// const calcAge = function (birthYear){
+//     return 2037 - birthYear;
+// }
+// const years = [1954, 1958, 1994, 2001, 2002];
+
+// const age1 = calcAge(years[0]);
+// const age2 = calcAge(years[1]);
+// const age3 = calcAge(years[years.length-1]);
+// console.log(age1, age2, age3);
+
+// const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length-1])];
+
+// console.log(ages)
+
+const friends = ['Halison', 'Eduardo', 'Luigi'];
+friends.push('Helion');
+console.log(friends);
+
+friends.unshift('Alex');
+console.log(friends);
+
+friends.pop();
+friends.pop();
+console.log(friends);
+
+friends.shift();
+console.log(friends)
